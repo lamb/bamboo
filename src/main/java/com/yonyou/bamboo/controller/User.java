06cd35afa23af1796e971457923795a54dccd7aa
Lamb <lambgao@me.com>
2012-09-23 18:49:39 +0800

    init

---
+++ src/main/java/com/yonyou/bamboo/controller/User.java
@@ -0,0 +1,40 @@
+package com.yonyou.bamboo.controller;
+
+import java.util.Date;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class User {
+    @NotEmpty
+    @Size(max=10,min=4)
+    private String name;
+    @NotNull
+    private int age;
+
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/yonyou/bamboo/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.yonyou.bamboo.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String toAdd() {
+        return "add";
+    }
+    
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String add(@Valid User user, BindingResult result) {
+        System.out.println(user.getName() + user.getAge() + user.getDate());
+        if(result.hasErrors()){
+            List<FieldError> errors= result.getFieldErrors();
+            for (FieldError error : errors) {
+                System.out.println(error.getDefaultMessage());
+            }
+        }
+        return "redirect:sucess.do";
+    }
+    
+    @RequestMapping("sucess")
+    public String sucess() {
+        return "sucess";
+    }
+    
+    
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), false));
+    }
+    
+}
+
