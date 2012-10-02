1f7b3c460c895a643f3fdf6d3fcc2426a7b427d2
Lamb <lambgao@me.com>
2012-10-02 15:56:08 +0800

    @Service 自动注入

--- src/main/java/com/yonyou/bamboo/controller/SignController.java
+++ src/main/java/com/yonyou/bamboo/controller/SignController.java
@@ -1,10 +1,10 @@
 package com.yonyou.bamboo.controller;
 
 import java.util.List;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -12,10 +12,14 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.yonyou.bamboo.model.User;
+import com.yonyou.bamboo.service.UserService;
 
 @Controller
 public class SignController {
 
+    @Autowired
+    private UserService userService;
+    
     @RequestMapping(value = "/signin", method = RequestMethod.POST)
     public String signin(@Valid User user, HttpServletResponse response, BindingResult result) {
         //TODO 前端页面验证
@@ -26,7 +30,7 @@ public class SignController {
             }
         } else {
             //TODO 登录后Cookie Session
-            if (true) {
+            if (userService.verifyPassword()) {
                 Cookie cookie = new Cookie("SU", user.getEmail() + ":" + user.getPassword());
                 cookie.setPath("/");
                 cookie.setMaxAge(2 * 3600);
--- src/main/java/com/yonyou/bamboo/service/UserService.java
+++ src/main/java/com/yonyou/bamboo/service/UserService.java
@@ -1,5 +1,13 @@
 package com.yonyou.bamboo.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    public boolean verifyPassword() {
+        System.out.println("verify");
+        return false;
+    }
 
 }
