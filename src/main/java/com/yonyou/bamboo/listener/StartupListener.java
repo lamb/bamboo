6bdb1a3011f95b06b24d0b079b113279c4085b41
noday <at1943@gmail.com>
2012-10-17 00:15:32 +0800

    启动加载配置逻辑

--- src/main/java/com/yonyou/bamboo/listener/StartupListener.java
+++ src/main/java/com/yonyou/bamboo/listener/StartupListener.java
@@ -1,11 +1,21 @@
 package com.yonyou.bamboo.listener;
 
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Properties;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.yonyou.bamboo.model.AppConfiguration;
+import com.yonyou.bamboo.repository.IAppRepository;
 
 /**
  * Application Lifecycle Listener implementation class StartupListener
@@ -13,6 +23,8 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  */
 public class StartupListener implements ServletContextListener {
 
+	private static Logger log = Logger.getLogger(StartupListener.class);
+	
 	private ServletContext context;
 	private ApplicationContext ctx;
     /**
@@ -26,12 +38,40 @@ public class StartupListener implements ServletContextListener {
      */
     public void contextInitialized(ServletContextEvent sce) {
     	ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(context = sce.getServletContext());
-        System.out.println("start ------------" + ctx);
+    	log.info("get spring ApplicationContext");
+    	setAppProperty();
+    	setSysProperty();
     }
 
 	/**
      * @see ServletContextListener#contextDestroyed(ServletContextEvent)
      */
     public void contextDestroyed(ServletContextEvent sce) {
+    }
+    
+    private void setAppProperty() {
+    	IAppRepository configRepo = ctx.getBean(IAppRepository.class);
+    	AppConfiguration config = configRepo.getConfig();
+    	if (config != null) {
+    		context.setAttribute(AppConfiguration.KEY_APPTITLE, config.getAppTitle());
+    		context.setAttribute(AppConfiguration.KEY_APPHOST, config.getAppHost());
+    		context.setAttribute(AppConfiguration.KEY_ADMINEMAIL, config.getAdminEmail());
+    		context.setAttribute(AppConfiguration.KEY_APPOWNER, config.getAppOwner());
+        	context.setAttribute(AppConfiguration.KEY_staticresourceversion, Calendar.getInstance().toString());
+        	Resource r = new ClassPathResource("web.properties");
+        	Properties p = new Properties();
+        	try {
+				p.load(r.getInputStream());
+				context.setAttribute(AppConfiguration.KEY_staticservepath, p.get(AppConfiguration.KEY_staticservepath));
+				context.setAttribute(AppConfiguration.KEY_servepath, p.get(AppConfiguration.KEY_servepath));
+			} catch (IOException e) {
+				log.error("配置读取失败", e);
+				// TODO 应该启动失败才行
+			}
+		}
+    }
+    
+    private void setSysProperty() {
+    	context.setAttribute(AppConfiguration.KEY_year, Calendar.getInstance().toString());
     }
 }
