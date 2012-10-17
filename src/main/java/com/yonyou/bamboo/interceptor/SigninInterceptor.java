6e24b21da87e754f2d838a5cbde8fc094c0016a5
Lamb <lambgao@me.com>
2012-10-17 18:18:50 +0800

    SigninInterceptor log

--- src/main/java/com/yonyou/bamboo/interceptor/SigninInterceptor.java
+++ src/main/java/com/yonyou/bamboo/interceptor/SigninInterceptor.java
@@ -2,14 +2,18 @@ package com.yonyou.bamboo.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class SigninInterceptor extends HandlerInterceptorAdapter{
+public class SigninInterceptor extends HandlerInterceptorAdapter {
+
+    private static Logger log = Logger.getLogger(SigninInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //HttpSession session = request.getSession();
-        //session.get
+        // HttpSession session = request.getSession();
+        // session.get
+        log.info("SigninInterceptor work");
         return super.preHandle(request, response, handler);
     }
 
