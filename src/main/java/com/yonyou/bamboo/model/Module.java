2914caf4269f6e61313e49cbab32be91b47d46fd
noday <at1943@gmail.com>
2012-10-22 23:26:01 +0800

    添加模块类

---
+++ src/main/java/com/yonyou/bamboo/model/Module.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yonyou.bamboo.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * bamboo Module
+ *
+ * @author <a href="http://www.noday.net">Noday</a>
+ * @version , 2012-10-22
+ * @since 
+ */
+public class Module implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private Long projectId;
+	private String name;
+	private Long createBy;
+	private Date createDate;
+	private Long updateBy;
+	private Date updateDate;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getCreateBy() {
+		return createBy;
+	}
+	public void setCreateBy(Long createBy) {
+		this.createBy = createBy;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Long getUpdateBy() {
+		return updateBy;
+	}
+	public void setUpdateBy(Long updateBy) {
+		this.updateBy = updateBy;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
