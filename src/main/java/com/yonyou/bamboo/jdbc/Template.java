9fba0b141d4b0aa40030541d738fab9f08deab21
Lamb <lambgao@me.com>
2012-10-25 17:06:03 +0800

    一个Template全搞定

--- src/main/java/com/yonyou/bamboo/jdbc/Template.java
+++ src/main/java/com/yonyou/bamboo/jdbc/Template.java
@@ -2,12 +2,36 @@ package com.yonyou.bamboo.jdbc;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.KeyHolder;
 
-public class Template extends NamedParameterJdbcTemplate {
+public class Template extends JdbcTemplate {
+
+    private NamedParameterJdbcTemplate namedJdbcTemplate;
+
+    public Template() {
+        super();
+    }
 
     public Template(DataSource dataSource) {
         super(dataSource);
+        initNamedJdbcTemplate();
+    }
+
+    public Template(DataSource dataSource, boolean lazyInit) {
+        super(dataSource, lazyInit);
+        initNamedJdbcTemplate();
+    }
+
+    public void initNamedJdbcTemplate() {
+        this.namedJdbcTemplate = new NamedParameterJdbcTemplate(this);
+    }
+
+    public int update(String sql, SqlParameterSource paramSource, KeyHolder generatedKeyHolder) throws DataAccessException {
+        return namedJdbcTemplate.update(sql, paramSource, generatedKeyHolder);
     }
 
 }
--- src/main/java/com/yonyou/bamboo/repository/impl/ProjectRepository.java
+++ src/main/java/com/yonyou/bamboo/repository/impl/ProjectRepository.java
@@ -3,12 +3,11 @@ package com.yonyou.bamboo.repository.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import com.yonyou.bamboo.jdbc.Template;
 import com.yonyou.bamboo.model.Project;
 import com.yonyou.bamboo.repository.IProjectRepository;
 
@@ -16,37 +15,35 @@ import com.yonyou.bamboo.repository.IProjectRepository;
 public class ProjectRepository implements IProjectRepository {
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
-    @Autowired
-    NamedParameterJdbcTemplate namedJdbcTemplate;
+    Template template;
 
     @Override
     public List<Project> list() {
-        return jdbcTemplate.query("select * from project", new BeanPropertyRowMapper<Project>(Project.class));
+        return template.query("select * from project", new BeanPropertyRowMapper<Project>(Project.class));
     }
 
     @Override
     public int save(Project project) {
         String sql = "insert into project(name, abbr, create_by, create_date) values(:name, :abbr, :createBy, now())";
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        namedJdbcTemplate.update(sql, new BeanPropertySqlParameterSource(project), keyHolder);
+        template.update(sql, new BeanPropertySqlParameterSource(project), keyHolder);
         return keyHolder.getKey().intValue();
     }
 
     @Override
     public Project query(int id) {
-        return jdbcTemplate.queryForObject("select * from project where id=?", new BeanPropertyRowMapper<Project>(Project.class), id);
+        return template.queryForObject("select * from project where id=?", new BeanPropertyRowMapper<Project>(Project.class), id);
     }
 
     @Override
     public int update(Project project) {
         String sql = "update project set name = ?, abbr = ?, modify_by = ?, modify_date = now() where id = ?";
-        return jdbcTemplate.update(sql, project.getName(), project.getAbbr(), project.getModifyBy(), project.getId());
+        return template.update(sql, project.getName(), project.getAbbr(), project.getModifyBy(), project.getId());
     }
 
     @Override
     public int delete(Project project) {
         String sql = "delete from project where id = ?";
-        return jdbcTemplate.update(sql, project.getId());
+        return template.update(sql, project.getId());
     }
 }
