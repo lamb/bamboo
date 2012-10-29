3c38b912174cfe7bbdb5726f3dbf0f0d16f8d524
Lamb <lambgao@me.com>
2012-10-29 18:20:11 +0800

    添加两个变量

--- src/test/java/com/yonyou/bamboo/util/ConstantsTest.java
+++ src/test/java/com/yonyou/bamboo/util/ConstantsTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 public class ConstantsTest {
 
+    public static final String BAMBOO_CONTEXT = "classpath:bamboo-context.xml";
+    public static final String BAMBOO_SERVLET_CONTEXT = "classpath:bamboo-servlet-context.xml";
+
     @Test
     public void test() {
         new Constants();
