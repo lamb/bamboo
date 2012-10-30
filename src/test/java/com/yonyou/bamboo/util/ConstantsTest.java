042e333a1c5ac5044d039d11454a1c5dd57775dc
Lamb <lambgao@me.com>
2012-10-30 17:19:12 +0800

    User常量用作测试的session

--- src/test/java/com/yonyou/bamboo/util/ConstantsTest.java
+++ src/test/java/com/yonyou/bamboo/util/ConstantsTest.java
@@ -2,12 +2,20 @@ package com.yonyou.bamboo.util;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import com.yonyou.bamboo.model.User;
 
 public class ConstantsTest {
 
     public static final String BAMBOO_CONTEXT = "classpath:bamboo-context.xml";
     public static final String BAMBOO_SERVLET_CONTEXT = "classpath:bamboo-servlet-context.xml";
     public static final String WEB_ROOT = "src/main/webapp";
+    public static final User USER = new User();
+
+    static {
+        USER.setId(-1);
+        USER.setEmail("test@email.com");
+        USER.setUsername("test");
+    }
 
     @Test
     public void test() {
